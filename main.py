b60397be324259a4995ee41373ff92175b4e94ca
RaymondLi0 <raymond_li@hotmail.fr>
2016-02-25 12:05:48 +0100

    Correction TPER_TEAM

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from preprocessing import preprocess
 
 
 #Load Data
-train_data = pd.read_csv('train_2011_2012.csv',header=0, sep=';')    
+#train_data = pd.read_csv('train_2011_2012.csv',header=0, sep=';')
+train_data = pd.read_csv('train_data_short.csv',header=0, sep=';')
 print train_data.head()
 
 #%%
