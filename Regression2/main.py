8f5036f14e08a0311f1868cbe6c528d77213451d
RaymondLi0 <raymond_li@hotmail.fr>
2016-03-04 21:24:46 +0100

    Regression without ASS_ASSIGNMENT dictionnaries

---
+++ Regression2/main.py
@@ -0,0 +1,144 @@
+import pandas as pd
+import numpy as np
+import datetime
+from preprocessing import preprocess
+from preprocessing import processDate
+import os
+import time
+
+from sklearn import cross_validation
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.grid_search import RandomizedSearchCV
+from sklearn.grid_search import GridSearchCV
+
+os.chdir('..')
+#==============================================================================
+# Algo avec régression, PAS de partition selon ASS_ASSIGNMENT
+#==============================================================================
+
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+submission = pd.read_csv('submission.txt',sep='\t',parse_dates=[0])
+
+#Load Data
+train_data = pd.read_csv('train_2011_2012.csv',header=0, sep=';',parse_dates=[0])
+#train_data = pd.read_csv('train_data_short.csv',header=0, sep=';',parse_dates=[1])
+#meteo2011 = pd.read_csv('meteo_2011.csv', sep=',',names=['date', 'dept_nb','city','temp_min','temp_max','wind_dir','precip','pressure_hPa'])
+#meteo2012 = pd.read_csv('meteo_2012.csv', sep=',',names=['date', 'dept_nb','city','temp_min','temp_max','wind_dir','precip','pressure_hPa'])
+#meteo = pd.read_csv('meteo_2011.csv', sep=',',names=['DATE', 'dept_nb','city','temp_min','temp_max','wind_dir','precip','pressure_hPa'], parse_dates=[0]).append(
+#pd.read_csv('meteo_2012.csv', sep=',',names=['DATE', 'dept_nb','city','temp_min','temp_max','wind_dir','precip','pressure_hPa'],parse_dates=[0]))
+meteo = pd.concat([pd.read_csv('meteo_2011.csv', sep=',',names=['date', 'dept_nb','city','temp_min','temp_max','wind_dir','precip','pressure_hPa'], parse_dates=[0]),(pd.read_csv('meteo_2012.csv', sep=',',names=['date', 'dept_nb','city','temp_min','temp_max','wind_dir','precip','pressure_hPa'], parse_dates=[0]))], ignore_index=True)
+
+
+#%%
+#==============================================================================
+# Data Pre processing
+#==============================================================================
+
+data, labels, meteo = preprocess(train_data, meteo)
+num_features = len(train_data.columns.values)
+
+
+#%%
+#==============================================================================
+# Learning Algorithm
+#==============================================================================
+print "Training ..."
+Regressors = {}
+
+start_time = time.time()
+
+#for ass in data:
+#    print ass
+##    data_train, data_test, labels_train, labels_test = cross_validation.train_test_split(data[ass].values, labels[ass])
+#    #Regressors[ass] = GaussianNB()
+##    Regressors[ass] = LogisticRegression()    
+##    Regressors[ass] = RandomForestRegressor()
+#    Regressors[ass] = GradientBoostingRegressor(n_estimators = 300, max_depth=7)
+##    rgs = GradientBoostingRegressor()
+##    GBRdict = {"n_estimators":[100,200,300],
+##               "max_depth":[3,5]}
+#
+#    # essayer de voir l'influence des paramètres
+#    # tester plusieurs valeurs
+#
+##    Regressors[ass] = GridSearchCV(rgs,param_grid=GBRdict,cv=5)
+#    Regressors[ass].fit(data[ass].values,labels[ass])    
+#        
+##    Regressors[ass].fit(data_train,labels_train)
+    
+regressor = GradientBoostingRegressor(n_estimators = 300, max_depth=7, verbose = 2)
+regressor.fit(data.values, labels)
+
+
+elapsed_time = time.time()-start_time
+print elapsed_time
+
+#%%
+#==============================================================================
+# Evaluation
+#==============================================================================
+#
+
+X = submission.copy()
+
+processDate(X,'DATE')
+X = pd.merge(X,meteo,on=['month','day','weekday','weekend','time','night'],how='left')
+
+for column in ['temp_min','temp_max','precip','pressure_hPa']:
+    X[column].fillna(np.mean(X[column]), inplace = True)
+    
+ass_list = np.unique(X['ASS_ASSIGNMENT'].values)
+for ass in ass_list :
+    X["AA_"+ass] = (X['ASS_ASSIGNMENT'] == ass)*1
+
+
+submission.prediction=regressor.predict(X.drop(['ASS_ASSIGNMENT','prediction'], axis=1))
+
+submission.prediction=submission.prediction.clip(lower = 0)
+submission.to_csv('new_results.txt',sep='\t',date_format='%Y-%m-%d %H:%M:%S.000',index=False )
+
+
+
+
+#print "EVALUATION"
+#
+#read = open('submission.txt', 'r')
+#w = open('results.txt', 'w')
+#
+##12409 lignes dans le fichier
+##test_data = np.zeros((12408,num_features))
+#
+##jours de la semaine pour les 12 jours concernés
+#days = ['MARDI','MERCREDI','LUNDI','LUNDI','SAMEDI','LUNDI','DIMANCHE','MARDI','JEUDI','MERCREDI','LUNDI','VENDREDI']
+#
+#i=0
+#for line in read :
+#    print "line "+str(i)
+#    if i!=0:
+#        [date, ass, sub] = line.split('\t')
+#        jour = days[int(date.split(' ')[0].split('-')[1])-1]
+#        time = int(date.split(' ')[1].split(':')[0])+float(date.split(' ')[1].split(':')[1])/60
+#        
+#        temp = data[ass].loc[data[ass]['TPER_HOUR']==time]
+#        temp = temp.loc[data[ass][jour]==1]
+#        if len(np.unique(temp['DATE'].values))!=0:
+#            moy = float(np.sum(temp['CSPL_RECEIVED_CALLS'].values))/len(np.unique(temp['DATE'].values))
+#        else :
+#            moy = 0
+#        
+#        w.write(date+'\t'+ass+'\t'+str(round(moy))+'\n')
+#        
+#    else :
+#        w.write(line)
+#        
+#        
+#    i=i+1
+#    
+#
+
---
+++ Regression2/preprocessing.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 23 14:28:26 2016
+
+@author: user
+"""
+#==============================================================================
+# Algo avec régression, PAS de partition selon ASS_ASSIGNMENT
+#==============================================================================
+
+import pandas as pd
+import numpy as np
+import datetime
+import time
+
+
+
+
+def getmonth(date) :
+    return date.month
+    
+def getday(date) : 
+    return date.day
+    
+def getweekday(date) : 
+    return date.dayofweek
+    
+def getweekend(date) : 
+    return (getweekday(date) >= 5)
+    
+def gettime(date) :
+    return(date.hour+date.minute/60.0)
+
+def getnight(date) : 
+    t = gettime(date)
+    return (t >= 23.5 or t <= 7)
+    
+def processDate(data, date_index):
+    data['month'] = data[date_index].map(getmonth)
+    data['day'] = data[date_index].map(getday)
+    data['weekday'] = data[date_index].map(getweekday)
+    data['weekend'] = data[date_index].map(getweekend)
+    data['time'] = data[date_index].map(gettime)    
+    data['night'] = data[date_index].map(getnight)
+    data.drop(date_index, axis=1, inplace=True)
+    
+    
+
+
+
+
+
+def preprocess(train_data,meteo) :
+    print "Start preprocess"
+    droplist =[]
+    
+    train_data.drop('DAY_WE_DS', axis=1,inplace=True)
+    train_data.drop('TPER_TEAM', axis=1,inplace=True)
+    train_data.drop('TPER_HOUR', axis=1,inplace=True)
+    train_data.drop('WEEK_END', axis=1,inplace=True)
+    
+#    pas de jours feriés ?
+    droplist.extend(['DAY_OFF','DAY_DS'])
+    droplist.extend(['ACD_COD','ACD_LIB'])
+    droplist.extend(['ASS_DIRECTORSHIP','ASS_PARTNER','ASS_POLE','ASS_SOC_MERE'])
+    droplist.extend(['ASS_BEGIN','ASS_COMENT','ASS_END'])
+    
+#    data_time = [float(t.split(' ')[1].split(':')[1])/60 for t in train_data['DATE']]
+    
+#    train_data['TPER_HOUR'] = train_data['TPER_HOUR'] + data_time
+    
+    droplist.extend(['SPLIT_COD','CSPL_CALLSOFFERED','CSPL_OUTFLOWCALLS','CSPL_INFLOWCALLS','CSPL_NOANSREDIR','CSPL_ACDCALLS',
+    'CSPL_ABNCALLS','CSPL_CONFERENCE','CSPL_TRANSFERED','CSPL_RINGCALLS','CSPL_DISCCALLS','CSPL_HOLDCALLS',
+    'CSPL_ACDAUXOUTCALLS','CSPL_HOLDABNCALLS','CSPL_MAXINQUEUE','CSPL_DEQUECALLS','CSPL_ACWINCALLS',
+    'CSPL_AUXINCALLS','CSPL_ACWOUTCALLS','CSPL_ACWOUTOFFCALLS','CSPL_ACWOUTADJCALLS','CSPL_AUXOUTCALLS',
+    'CSPL_AUXOUTOFFCALLS','CSPL_AUXOUTADJCALLS','CSPL_INTRVL','CSPL_OUTFLOWTIME','CSPL_DEQUETIME','CSPL_I_ACDTIME',
+    'CSPL_DISCTIME','CSPL_HOLDTIME','CSPL_ABNTIME','CSPL_I_STAFFTIME','CSPL_ANSTIME','CSPL_I_RINGTIME',
+    'CSPL_RINGTIME','CSPL_ACDTIME','CSPL_I_AVAILTIME','CSPL_ACWTIME','CSPL_I_ACWTIME','CSPL_I_OTHERTIME',
+    'CSPL_ACWINTIME','CSPL_I_ACWINTIME','CSPL_AUXINTIME','CSPL_I_AUXINTIME','CSPL_ACWOUTIME','CSPL_I_ACWOUTTIME',
+    'CSPL_ACWOUTOFFTIME','CSPL_AUXOUTTIME','CSPL_I_AUXOUTTIME','CSPL_AUXOUTOFFTIME','CSPL_SERVICELEVEL',
+    'CSPL_ACCEPTABLE','CSPL_SLVLOUTFLOWS','CSPL_SLVLABNS','CSPL_ABNCALLS1','CSPL_ABNCALLS2','CSPL_ABNCALLS3',
+    'CSPL_ABNCALLS4','CSPL_ABNCALLS5','CSPL_ABNCALLS6','CSPL_ABNCALLS7','CSPL_ABNCALLS8','CSPL_ABNCALLS9',
+    'CSPL_ABNCALLS10','CSPL_MAXSTAFFED','CSPL_INCOMPLETE','CSPL_ABANDONNED_CALLS','CSPL_CALLS'])
+    train_data.drop(droplist, axis=1,inplace=True)
+    
+    if 'Unnamed: 0' in train_data.columns.values:
+        train_data.drop('Unnamed: 0', axis=1,inplace=True)
+        
+    processDate(train_data,'DATE')
+        
+#%%
+#   traitement de météo
+        
+    print "Preprocess meteo"
+    
+    def addSemiHours(date) :
+        return date + datetime.timedelta(minutes=30)
+    
+#    def formatDate(date) :
+#        return date+":00.000"
+        
+    meteo = meteo[meteo['city']=='Paris-Montsouris']
+    meteo.drop(['dept_nb','city','wind_dir'], axis=1,inplace=True)
+    
+    meteo_temp = meteo.copy(deep=True)
+    meteo_temp['date'] = meteo['date'].map(addSemiHours)
+    
+    meteo = meteo.append(meteo_temp)
+    meteo.sort(['date'], inplace = True)    
+    del meteo_temp    
+        
+    processDate(meteo,'date')
+    
+    
+    
+#%%
+#   Enlever les ass_assignment non traités    
+    print "Removing non-treated ASS_ASSIGNMENT"
+        
+    non_traite = ['A DEFINIR', 'AEVA', 'DOMISERVE', 'Divers','Evenements', 'FO Remboursement', 'Finances PCX',
+       'IPA Belgique - E/A MAJ','Juridique', 'KPT', 'LifeStyle','Maroc - Génériques', 'Maroc - Renault',
+       'Medicine', 'NL Médical', 'NL Technique','Réception', 'TAI - CARTES', 'TAI - PANNE MECANIQUE',
+       'TAI - PNEUMATIQUES', 'TAI - RISQUE', 'TAI - RISQUE SERVICES','TAI - SERVICE', 'TPA',
+       'Technical', 'Technique Belgique', 'Technique International','Truck Assistance']
+    for ass in non_traite:
+        train_data = train_data[train_data['ASS_ASSIGNMENT']!=ass]
+        
+        
+#%%
+    print "Grouping data"
+
+    groups = train_data.groupby(['month','day','weekday','weekend','time','night','ASS_ASSIGNMENT']).sum().reset_index()
+    
+#%%
+#Concaténer météo et groups
+    print "Concatenating meteo and groups"
+    
+    groups = pd.merge(groups,meteo,on=['month','day','weekday','weekend','time','night'],how='left')
+#%%    
+    
+    for column in ['temp_min','temp_max','precip','pressure_hPa']:
+        groups[column].fillna(np.mean(groups[column]), inplace = True)
+        
+#%%
+#  partitionner les données selon leur ass_assignment
+    print "New ass_assignment columns"
+    ass_list = np.unique(groups['ASS_ASSIGNMENT'].values)
+    
+#    data = {}
+#    labels = {}
+#    for ass in ass_list :
+#        data[ass] = groups.loc[groups['ASS_ASSIGNMENT']==ass]
+#        labels[ass] = groups['CSPL_RECEIVED_CALLS'].loc[groups['ASS_ASSIGNMENT']==ass]
+#        data[ass].drop(['ASS_ASSIGNMENT','CSPL_RECEIVED_CALLS'], axis=1, inplace = True)
+    
+#    OU BIEN : on crée plusieurs colonnes
+    for ass in ass_list :
+        print "creating column : AA_"+ass
+        groups["AA_"+ass] = (groups['ASS_ASSIGNMENT'] == ass)*1
+        
+    labels = groups['CSPL_RECEIVED_CALLS']
+    groups.drop(['ASS_ASSIGNMENT','CSPL_RECEIVED_CALLS'], axis=1, inplace = True)
+    
+    
+#%%
+    
+    
+    return groups, labels, meteo
